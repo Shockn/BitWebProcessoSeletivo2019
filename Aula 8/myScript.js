105244a70ca3cf9b4e9d48f35f24f5198a384407
Yuri <yurideoliveirac@gmail.com>
2019-11-27 15:00:13 -0300

    Arrumadas as aulas

--- Aula 8/myScript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    M.AutoInit();
-    var elems = document.querySelectorAll('.carousel');
-    var options = { dist: 0, padding: 20, numVisible: 6 };
-    var instances = M.Carousel.init(elems, options);
-  });

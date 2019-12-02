c5dfe3671bfb5766a7d3c27f526aa8a017136b05
Yuri <yurideoliveirac@gmail.com>
2019-12-02 18:02:09 -0300

    Começo do design do site

--- Exercícios/Projeto Final/script.js
+++ Exercícios/Projeto Final/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
     var elems = document.querySelectorAll('.carousel');
-    var options = { dist: 0, padding: 20, numVisible: 5};
+    var options = { dist: 0, padding: 140, numVisible: 3, duration: 100 };
     var instances = M.Carousel.init(elems, options);
 });

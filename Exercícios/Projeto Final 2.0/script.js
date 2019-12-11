5f5e6d585d94fa6861b7f7f63f8a82229e30fa67
Yuri <yurideoliveirac@gmail.com>
2019-12-11 14:01:18 -0300

    Diminuído o código do javaScript

--- Exercícios/Projeto Final 2.0/script.js
+++ Exercícios/Projeto Final 2.0/script.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit()
     var elems = document.querySelectorAll('.carousel');
+    var elems2 = document.querySelectorAll('.sidenav');
     var options = { indicator: true, dist: 0, padding: 50, numVisible: 3, duration: 100};
+    var options2 = { edge: 'right', draggable: true };
     var instances = M.Carousel.init(elems, options);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var options = { edge: 'right', draggable: true };
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems2, options2)
 });

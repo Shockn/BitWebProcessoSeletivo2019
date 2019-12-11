513492474c997279dc70702cd5b945a3275ac8ba
Yuri <yurideoliveirac@gmail.com>
2019-12-11 11:55:15 -0300

    Inserida a sidenav (tinha esquecido de botar)

--- Exercícios/Projeto Final 2.0/script.js
+++ Exercícios/Projeto Final 2.0/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    M.AutoInit();
     var elems = document.querySelectorAll('.carousel');
     var options = { indicator: true, dist: 0, padding: 50, numVisible: 3, duration: 100};
     var instances = M.Carousel.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit()
+    var elems = document.querySelectorAll('.sidenav');
+    var options = { edge: 'right', draggable: true };
+    var instances = M.Sidenav.init(elems, options);
 });
